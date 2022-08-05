d8b2cc124c123493ffacb28a241ccd48b1f483bd
Fabio Rotondo <fabio.rotondo@gmail.com>
2022-08-05 15:30:31 +0200

    configuration for building

---
+++ fsoft-components/index.ts
@@ -0,0 +1,8 @@
+/* export all components in fsoft-components */
+
+import BGImage from './BGImage';
+import DNDTree from './DNDTree';
+import NonSSR from './NonSSR';
+import VideoBackground from './VideoBackground';
+
+export { BGImage, DNDTree, NonSSR, VideoBackground };
--- index.js
+++
@@ -1,8 +0,0 @@
-/* export all components in fsoft-components */
-
-import BGImage from './fsoft-components/BGImage';
-import DNDTree from './fsoft-components/DNDTree';
-import NonSSR from './fsoft-components/NonSSR';
-import VideoBackground from './fsoft-components/VideoBackground';
-
-export { BGImage, DNDTree, NonSSR, VideoBackground };
