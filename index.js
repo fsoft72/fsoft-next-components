db807c09e51a178fc7d133357e4e351bf3138eee
Fabio Rotondo <fabio.rotondo@gmail.com>
2022-08-05 12:03:04 +0200

    ADD: exports

---
+++ index.js
@@ -0,0 +1,8 @@
+/* export all components in fsoft-components */
+
+import BGImage from './fsoft-components/BGImage';
+import DNDTree from './fsoft-components/DNDTree';
+import NonSSR from './fsoft-components/NonSSR';
+import VideoBackground from './fsoft-components/VideoBackground';
+
+export { BGImage, DNDTree, NonSSR, VideoBackground };
